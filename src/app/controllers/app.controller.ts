d6d41c6df0a25f9f4ac4baaf4deee302df701aaa
clb158 <93491190+clb158@users.noreply.github.com>
2024-02-27 14:24:22 -0600

    still having errors with cb function working with Jack Anderson

--- src/app/controllers/app.controller.ts
+++ src/app/controllers/app.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, Router, request } from "express";
 import { pino } from "pino";
 const passport = require("passport");
 const cookieSession = require("cookie-session");
+import db from "../firebase";
 require("./passport");
 
 export class AppController {
@@ -22,6 +23,7 @@ export class AppController {
 
     this.router.use(passport.initialize());
     this.router.use(passport.session());
+
     // Serve the home page
     this.router.get("/", (req: Request, res: Response) => {
       try {
@@ -35,25 +37,26 @@ export class AppController {
     // Serve the calendar page
     this.router.get("/calendar", (req: Request, res: Response) => {
       try {
-        // Render the "home" template as HTML
+        // Render the "calendar" template as HTML
         res.render("calendar");
       } catch (err) {
         this.log.error(err);
       }
     });
 
-    this.router.get("/failed", (req: Request, res: Response) => {
-      res.send("Failed");
-    });
-
     this.router.get("/profile", (req: Request, res: Response) => {
-      res.send(`Welcomes`);
+      try {
+        // Render the "profile" template as HTML
+        res.render("profile");
+      } catch (err) {
+        this.log.error(err);
+      }
     });
 
     this.router.get(
       "/google",
       (req: Request, res: Response, next: any) => {
-        console.log("Google");
+        console.log("Google route working");
         next();
       },
       passport.authenticate("google", { scope: ["email", "profile", "openid"] })
@@ -61,11 +64,11 @@ export class AppController {
 
     this.router.get(
       "/google/callback",
-      passport.authenticate("google", { failureRedirect: "/login" }),
-      function (req: Request, res: Response) {
+      passport.authenticate("google", { failureRedirect: "/" }),
+      (req: Request, res: Response) => {
         // Successful authentication, redirect to profile.
-        console.log("Authentication Called");
-        res.redirect("/");
+        console.log("google callback working");
+        res.redirect("/profile");
       }
     );
   }
--- src/app/controllers/passport.js
+++ src/app/controllers/passport.js
@@ -3,23 +3,16 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import db from "../firebase.ts";
 
 // These need to be changed to environment variables
-const GOOGLE_CLIENT_ID = "949890250152-b3cffqki491rr7nc1s11d27t0of58opl.apps.googleusercontent.com";
+const GOOGLE_CLIENT_ID =
+  "949890250152-b3cffqki491rr7nc1s11d27t0of58opl.apps.googleusercontent.com";
 const GOOGLE_CLIENT_SECRET = "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";
-
-passport.serializeUser(function (user, cb) {
-  // cb(null, user);
-});
-
-passport.deserializeUser(function (user, cb) {
-  // cb(null, user);
-});
 
 passport.use(
   new GoogleStrategy(
     {
       clientID: GOOGLE_CLIENT_ID,
       clientSecret: GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/google/callback",
+      callbackURL: "/google/callback",
     },
     async function (accessToken, refreshToken, profile, email, openid, cb) {
       const userRef = db.collection("users").doc(`${profile.displayName}`);
@@ -27,19 +20,32 @@ passport.use(
       const doc = await userRef.get();
       console.log("Doc working");
       if (!doc.exists) {
-        console.log("No such document!");
+        console.log("user does not exist");
         const newUser = {
           profileId: profile.id,
           name: profile.displayName,
           email: profile.emails[0],
         };
-        userRef.set(newUser);
-        // .then(() => cb(null, newUser));
-        // .catch((err) => cb(err));
+        userRef.set(newUser).then(() => {
+          console.log("user created");
+          cb(null, newUser);
+        });
       } else {
-        console.log("Document data:", doc.data());
-        // cb(null, doc.data());
+        console.log("User already exists.\nDocument data:", doc.data());
+        cb(null, doc);
       }
     }
   )
 );
+
+passport.serializeUser(function (profile, cb) {
+  console.log("Serialized user");
+  cb(null, profile.id);
+});
+
+passport.deserializeUser(function (id, cb) {
+  userRef.where("profileId", "==", id).then((profile) => {
+    console.log("deserialized user");
+    cb(null, profile);
+  });
+});
