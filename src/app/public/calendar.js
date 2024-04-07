09925e5f61576cfdac68391cd2ab28625fd3c50a
clb158 <93491190+clb158@users.noreply.github.com>
2024-04-07 18:50:33 -0500

    google calendar displaying

--- src/app/public/calendar.js
+++ src/app/public/calendar.js
@@ -4,6 +4,11 @@ var events = [];
 document.addEventListener('DOMContentLoaded', function() {
       var calendarEl = document.getElementById('calendar');
       calendar = new FullCalendar.Calendar(calendarEl, {
+  		googleCalendarApiKey: 'AIzaSyBSI3DVYbn6WY3tV3KTrPIEFBlbL0t0s6o',
+		events: {
+			googleCalendarId: 'c_3f89c65c96906b0e35da55a80a8ecd7ba5babdd9d54f4fdaddb1da6230766718@group.calendar.google.com',
+			className: 'gcal-event' // an option!
+		},
         headerToolbar: {
 			start: 'dayGridMonth,timeGridWeek,timeGridDay',
 			center: 'title',
@@ -23,7 +28,6 @@ document.addEventListener('DOMContentLoaded', function() {
 			},
 		},
       }); 
-	  addevent();
       calendar.render();
 });
 
