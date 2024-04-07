6c97f32b4ad7c604962f1ec19775ba1dab98b531
Daniel Miller <dm193@students.uwf.edu>
2024-04-07 18:03:15 -0500

    file structure and shells built

---
+++ src/app/Schemas/ajv-instance.js
@@ -0,0 +1,6 @@
+import Ajv, { JSONSchemaType } from "ajv";
+import addFormats from 'ajv-formats';
+
+const ajv = new Ajv({ allErrors: true });
+addFormats(ajv);
+export default ajv;
---
+++ src/app/Schemas/evaluations.js
@@ -0,0 +1,8 @@
+const Ajv = require("ajv");
+const addFormats = require("ajv-formats");
+
+// Initialize AJV
+const ajv = new Ajv();
+addFormats(ajv);
+
+ajv.addSchema(evaluations, 'evaluations');
