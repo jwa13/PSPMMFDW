8db976dece9b32013cf7130e8d120128b11871fa
Daniel Miller <dm193@students.uwf.edu>
2024-04-07 18:04:24 -0500

    update

--- src/app/Schemas/evaluations.js
+++ src/app/Schemas/evaluations.js
@@ -1,8 +1,24 @@
-const Ajv = require("ajv");
-const addFormats = require("ajv-formats");
+const Ajv = require('ajv');
+const addFormats = require('ajv-formats');
 
 // Initialize AJV
 const ajv = new Ajv();
 addFormats(ajv);
 
-ajv.addSchema(evaluations, 'evaluations');
+const eval_1 = {
+    '$schema': 'http://json-schema.org/draft-07/schema#',
+    'title': 'Evaluations',
+    'type': 'object',
+    'properties': {
+        'coach': { 'type': 'string' },
+        'comments': { 'type': 'string' },
+        'date': { 'type': 'string', 'format': 'date-time' },
+        'moundVelo': { 'type': 'number' },
+        'pitching': { 'type': 'boolean' },
+        'pulldownVelo': { 'type': 'number' },
+        'userId': { 'type': 'string' }
+    },
+    'required': ['coach', 'comments', 'date', 'moundVelo', 'pitching', 'pulldownVelo', 'userId']
+};
+
+ajv.addSchema(eval_1, 'eval_1');
--- src/app/Schemas/workouts.js
+++ src/app/Schemas/workouts.js
@@ -1,8 +1,25 @@
-const Ajv = require("ajv");
-const addFormats = require("ajv-formats");
+const Ajv = require('ajv');
+const addFormats = require('ajv-formats');
 
 // Initialize AJV
 const ajv = new Ajv();
 addFormats(ajv);
 
-ajv.addSchema(workouts, 'workouts');
+const workout_1 = {
+    '$schema': 'http://json-schema.org/draft-07/schema#',
+    'title': 'Workouts',
+    'type': 'object',
+    'properties': {
+        'coach': { 'type': 'string' },
+        'comments': { 'type': 'array', 'items': { 'type': 'string' } },
+        'exercise': { 'type': 'array', 'items': { 'type': 'string' } },
+        'reps': { 'type': 'array', 'items': { 'type': 'string' } },
+        'sets': { 'type': 'array', 'items': { 'type': 'string' } },
+        'userId': { 'type': 'string' },
+        'video': { 'type': 'array', 'items': { 'type': 'string' } },
+        'weight': { 'type': 'array', 'items': { 'type': 'string' } }
+    },
+    'required': ['coach', 'comments', 'exercise', 'reps', 'sets', 'userId', 'video', 'weight']
+};
+
+ajv.addSchema(workout_1, 'workout_1');
