26ece2e5fca7c9ee6fc5539c0627bae364b8afe5
clb158 <93491190+clb158@users.noreply.github.com>
2024-04-07 19:24:51 -0500

    moved js from calendar to controller

--- src/app/controllers/dataController.js
+++ src/app/controllers/dataController.js
@@ -190,6 +190,34 @@ const dataController = {
 		}
 	},
 
+	processCreateCalendar: () => {
+		document.addEventListener('DOMContentLoaded', function() {
+			var calendarEl = document.getElementById('calendar');
+			calendar = new FullCalendar.Calendar(calendarEl, {
+			  headerToolbar: {
+				  start: 'dayGridMonth,timeGridWeek,timeGridDay',
+				  center: 'title',
+				  end: 'prevYear,prev,next,nextYear',
+			  },
+			  footerToolbar: {
+				  start: 'custom1',
+				  center: '',
+				  end: 'prev,next',
+			  },
+			  customButtons: {
+				  custom1: {
+					  text: 'Schedule an Appointment',
+					  click: function () {
+						  alert('clicked custom button 1!');
+					  },
+				  },
+			  },
+			}); 
+			addevent();
+			calendar.render();
+	  });
+	}
+
 };
 
 export default dataController;
--- src/app/public/calendar.js
+++ src/app/public/calendar.js
@@ -1,40 +1,4 @@
-var calendar = null;
-var events = [];
+import processData from '../controllers/dataController';
 
-document.addEventListener('DOMContentLoaded', function() {
-      var calendarEl = document.getElementById('calendar');
-      calendar = new FullCalendar.Calendar(calendarEl, {
-  		googleCalendarApiKey: 'AIzaSyBSI3DVYbn6WY3tV3KTrPIEFBlbL0t0s6o',
-		events: {
-			googleCalendarId: 'c_3f89c65c96906b0e35da55a80a8ecd7ba5babdd9d54f4fdaddb1da6230766718@group.calendar.google.com',
-			className: 'gcal-event' // an option!
-		},
-        headerToolbar: {
-			start: 'dayGridMonth,timeGridWeek,timeGridDay',
-			center: 'title',
-			end: 'prevYear,prev,next,nextYear',
-		},
-		footerToolbar: {
-			start: 'custom1',
-			center: '',
-			end: 'prev,next',
-		},
-		customButtons: {
-			custom1: {
-				text: 'Schedule an Appointment',
-				click: function () {
-					alert('clicked custom button 1!');
-				},
-			},
-		},
-      }); 
-      calendar.render();
-});
+processData.processCreateCalendar;
 
-function addevent() {
-      calendar.addEvent({
-         start: '2024-04-23T14:00:00',
-         end: '2024-04-23T15:00:00',
-         title: 'Test'
-      });
-}
